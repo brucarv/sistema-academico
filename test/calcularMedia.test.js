d1aea5386c39ad8df72ff9102a34748f99e35e03
Bruna Carvalho <brucarv.nah@gmail.com>
2024-11-13 20:45:35 -0300

    updating files

--- test/calcularMedia.test.js
+++ test/calcularMedia.test.js
@@ -1,50 +1,24 @@
 const { calcularMedia } = require("../src/calcularMedia");
 
 describe("Teste de cálculo de média", () => {
-  // Caso de teste: Notas válidas de 1UP e 2UP
   test("Notas válidas de 1UP e 2UP", () => {
-    const nota1 = 7.0;
-    const nota2 = 8.0;
-
-    const resultado = calcularMedia(nota1, nota2);
-
-    expect(resultado).toBe(7.5);
+    expect(calcularMedia(7.0, 8.0)).toBe(7.5);
   });
 
   test("Notas iguais de 1UP e 2UP", () => {
-    const nota1 = 5.0;
-    const nota2 = 5.0;
-
-    const resultado = calcularMedia(nota1, nota2);
-
-    expect(resultado).toBe(5.0);
+    expect(calcularMedia(5.0, 5.0)).toBe(5.0);
   });
 
   test("Nota mínima e máxima (0.0 e 10.0)", () => {
-    const nota1 = 0.0; // Mínima
-    const nota2 = 10.0; // Máxima
-
-    const resultado = calcularMedia(nota1, nota2);
-
-    expect(resultado).toBe(5.0);
+    expect(calcularMedia(0.0, 10.0)).toBe(5.0);
   });
 
   test("Nota de 1UP no limite inferior e nota de 2UP no limite superior", () => {
-    const nota1 = 0.0; // Limite inferior
-    const nota2 = 7.0; // Limite superior
-
-    const resultado = calcularMedia(nota1, nota2);
-
-    expect(resultado).toBe(3.5);
+    expect(calcularMedia(0.0, 7.0)).toBe(3.5);
   });
 
   test("Nota de 1UP no limite inferior e nota de 2UP no limite superior", () => {
-    const nota1 = 8.0;
-    const nota2 = 10.0;
-
-    const resultado = calcularMedia(nota1, nota2);
-
-    expect(resultado).toBe(9.0);
+    expect(calcularMedia(8.0, 10.0)).toBe(9.0);
   });
 
   test("Notas inválidas fora do intervalo", () => {
